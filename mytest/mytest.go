9a93f31fbb9572da6c75e544f8f8580ca6d89394
VVVVictorJ <VictorRobin1097@gmail.com>
2023-04-30 17:24:00 +0800

    初始创建Go练习存储库

---
+++ mytest/mytest.go
@@ -0,0 +1,6 @@
+package mytest
+
+func Add(number1 int, number2 int)(result int){
+	result = number1 + number2
+	return result
+}
---
+++ simple-app/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"Go_Practice/mytest"
+	"fmt"
+)
+
+func main() {
+	name := "Go Developers"
+	fmt.Println("Azure for", name)
+	fmt.Print(mytest.Add(1, 2))
+}
