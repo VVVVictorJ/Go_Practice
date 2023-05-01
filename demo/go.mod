b19827966672dc9ad81756a50a784fa6f2587631
VVVVictorJ <VictorRobin1097@gmail.com>
2023-05-01 23:05:53 +0800

    学习如何获取自己封装的包

--- demo/go.mod
+++ demo/go.mod
@@ -1,3 +1,5 @@
 module main
 
 go 1.20
+
+require github.com/VVVVictorJ/GOPACKAGETEST v1.0.0
--- demo/main.go
+++ demo/main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"main/mytest"
 	"main/mytest/ModuleA"
+	ve "github.com/VVVVictorJ/GOPACKAGETEST"
 )
 
 func main(){
 	fmt.Println("Test")
 	fmt.Println(mytest.Test(2, 1))
 	fmt.Println(modulea.TestA(2, 1))
+	fmt.Println(ve.Version())
 }
